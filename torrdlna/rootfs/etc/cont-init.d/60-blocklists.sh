75f15966d9202b45d0a64a08c253bc9d3b478b75
Botond Barabas <bboti86@gmail.com>
2017-12-29 01:51:56 +0100

    making sure file exist before trying to write into it

--- torrdlna/rootfs/etc/cont-init.d/60-blocklists.sh
+++ torrdlna/rootfs/etc/cont-init.d/60-blocklists.sh
@@ -8,4 +8,5 @@ source /usr/lib/hassio-addons/base.sh
 
 # Download lists, unpack and filter, write to stdout
 echo "Generating blocklist:"
+touch /root/.config/transmission-daemon/blocklists/torrent_blocklist_url.txt
 curl -s https://www.iblocklist.com/lists.php | sed -n "s/.*value='\(http[^']*\)'.*/'\1'/p" | xargs wget -O - | gunzip | egrep -v '^#' > /root/.config/transmission-daemon/blocklists/torrent_blocklist_url.txt
