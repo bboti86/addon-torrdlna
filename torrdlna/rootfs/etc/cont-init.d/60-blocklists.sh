ebada14cb0b12dd588af46b43eb57ad2f8c637f6
Botond Barabas <bboti86@gmail.com>
2017-12-29 01:30:17 +0100

    initial commit
    v 0.1.0
    TODO: readme!

---
+++ torrdlna/rootfs/etc/cont-init.d/60-blocklists.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# Community Hass.io Add-ons: torrdlna
+# Ensures necessary blocklists are downloaded
+# ==============================================================================
+# shellcheck disable=SC1091
+source /usr/lib/hassio-addons/base.sh
+
+# Download lists, unpack and filter, write to stdout
+echo "Generating blocklist:"
+curl -s https://www.iblocklist.com/lists.php | sed -n "s/.*value='\(http[^']*\)'.*/'\1'/p" | xargs wget -O - | gunzip | egrep -v '^#' > /root/.config/transmission-daemon/blocklists/torrent_blocklist_url.txt
